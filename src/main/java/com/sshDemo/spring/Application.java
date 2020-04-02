9a5892acef79b6a57708df1eca8c774c48b188d2
syh1007 <1053771808@qq.com>
2020-04-02 17:00:38 +0800

    初次提交

---
+++ src/main/java/com/sshDemo/spring/Application.java
@@ -0,0 +1,16 @@
+package com.sshDemo.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author changling
+ * @creator 2020-04-02 15:51
+ */
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
---
+++ src/main/java/com/sshDemo/spring/controllers/HelloWorld.java
@@ -0,0 +1,23 @@
+package com.sshDemo.spring.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author changling
+ * @creator 2020-04-02 16:00
+ */
+@RestController
+public class HelloWorld {
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public Map helloWorld() {
+        Map<String, Object> ret = new HashMap<>();
+        ret.put("ret", "Hello World");
+        return ret;
+    }
+
+}
